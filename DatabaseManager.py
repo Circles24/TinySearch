077d154620ad0b7be9c168e3c13a8dd5fc027777
Suraj Singh Rana <surajsinghrana7417@gmail.com>
2019-08-31 12:16:25 +0530

    database manager added

---
+++ DatabaseManager.py
@@ -0,0 +1,48 @@
+import pymongo
+
+class DatabaseManager:
+
+	__instanceRef = None
+
+	def __init__(self):
+
+		self.db = pymongo.MongoClient("mongodb://localhost:27017/")['TinySearch']
+
+		self.workColl = self.db['workColl']
+		self.searchColl = self.db['searchColl']
+		self.imgColl = self.db['imgColl']
+		self.wikiColl = self.db['wikiColl']
+		self.webColl = self.db['webColl']
+
+		DatabaseManager.__instanceRef = self
+
+
+	def getDatabaseManager():
+
+		if DatabaseManager.__instanceRef is None:
+
+			DatabaseManager()
+
+		return DatabaseManager.__instanceRef
+
+
+	def getWorkColl(self):
+
+		return self.workColl 
+
+	def getSearchColl(self):
+
+		return self.searchColl
+
+	def getImgColl(self):
+
+		return self.imgColl
+
+	def getWikiColl(self):
+
+		return self.wikiColl
+
+	def getWebColl(self):
+
+		return self.webColl
+
